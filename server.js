3ad07ba08794cd1ecb6787e3e4b12134bcd9d390
abhisheks <abhisheks@mediawareonline.com>
2018-10-18 19:22:07 +0530

    Set up start script and Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -13,7 +15,7 @@ app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now} ${req.method} ${req.path}`;
     console.log(log);
-    fs.appendFile('Server.log ', log + '\n', (err) => {
+    fs.appendFile('server.log ', log + '\n', (err) => {
         if (err) {
             console.log('Unable to append to server.log');
         }
@@ -66,6 +68,6 @@ app.get("/bad", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
